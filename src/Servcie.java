36e550bed9fbc35f08a58e1df09fbb7d7985fbe9
palm000palmovich <ivankiselev6534@gmail.com>
2024-09-17 11:52:36 +0300

    all Exceptions

---
+++ src/Servcie.java
@@ -0,0 +1,6 @@
+public class Servcie {
+    public static void Check(String login, String password, String confirmPassword)
+            throws WrongPasswordException, WrongLoginException{
+    }
+
+}
