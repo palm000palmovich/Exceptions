487f40b1bdfe5bc240198099337d0643fcadadf3
palm000palmovich <ivankiselev6534@gmail.com>
2024-09-17 13:50:43 +0300

    full_project

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Servcie ser = new Servcie();
+        try{
+            ser.Check("123", "huihiubnseyucb3", "huihiubnseyucb3");
+            System.out.println("Все данные корректны.");
+        } catch (WrongLoginException | WrongPasswordException e){
+            System.out.println("Ошибка входа: " + e.getMessage());
+        }
     }
 }
--- src/Servcie.java
+++ src/Servcie.java
@@ -1,6 +1,37 @@
 public class Servcie {
-    public static void Check(String login, String password, String confirmPassword)
-            throws WrongPasswordException, WrongLoginException{
+    public static void Check(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException{
+
+        //Проверка логина
+        String logRule = "qwertyuiopasdfghjklzxcvbnm_QWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+        int logCount = 0;
+        for (char log: login.toCharArray()){
+            for (char logR: logRule.toCharArray()){
+                if (logR == log){
+                    logCount++;
+                }
+            }
+            }
+
+        if (login.length() > 20 || logCount < login.length()){
+            throw new WrongLoginException("Недопустимый логин!");
+        }
+
+        //Проверка пароля
+        String pasRule = logRule;
+        int pasCount = 0;
+        for (char pas: password.toCharArray()){
+            for (char pasR: pasRule.toCharArray()){
+                if (pasR == pas){
+                    pasCount++;
+                }
+            }
+        }
+
+        if (password.length() > 20 || pasCount < password.length() || !password.equals(confirmPassword)){
+            throw new WrongLoginException("Недопустимый пароль!");
+        }
+
     }
 
-}
+    }
+
